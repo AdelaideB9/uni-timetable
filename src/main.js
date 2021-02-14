e797cad2fbb5cfd6f7b5370eed6c4a3911c4dd48
LachlanS7 <lachlanschilling73@gmail.com>
2021-02-14 23:09:26 +1030

    Adding Buefy

--- src/main.js
+++ src/main.js
@@ -2,8 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+// internal icons
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
 
 Vue.config.productionTip = false
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas",
+  customIconPacks: {
+    fas: {
+      sizes: {
+        default: "lg",
+        "is-small": "1x",
+        "is-medium": "2x",
+        "is-large": "3x"
+      },
+      iconPrefix: ""
+    }
+  }
+})
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-button @click="clickMe">Click Me</b-button>
+    <b-datepicker v-model="date" 
+      inline 
+      :unselectable-days-of-week="[0, 6]">
+    </b-datepicker>
   </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                date: new Date()
+            }
+        }
+    }
+</script>
