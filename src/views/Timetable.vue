99a9f4ce0545bec14e59c6f3cf4e2ac05d73aaa2
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2021-02-16 13:24:18 +1030

    We have a timetable!

--- src/views/Timetable.vue
+++ src/views/Timetable.vue
@@ -10,14 +10,43 @@
           :unselectable-days-of-week="[0, 6]"
         >
         </b-datepicker>
-        <br>
-        <pre>{{ timetable }}</pre>
+        <br />
+        <!-- <pre>{{ timetable }}</pre> -->
+        <div id="badidea"></div>
       </div>
     </div>
   </div>
 </template>
 
+<style lang="scss">
+table {
+  table-layout: fixed;
+  width: 100%;
+  height: 100%;
+  font-size: 12px;
+
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  border-collapse: unset !important;
+  border-spacing: 10px !important;
+}
+
+table, td, th { border: none !important; }
+
+tr {
+  height: 60px;
+}
+
+.altbdr {
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
+
 <script>
+const seedrandom = require('seedrandom');
 import TopProgress from "@/components/top-progress.vue";
 
 export default {
@@ -29,8 +58,14 @@ export default {
     async loadTimetable() {
       this.$refs.topProgress.start();
       this.errorText = "";
-      let url = encodeURIComponent(`student/Week.asp?term=4110&career=UGRD&dt=${this.date.getDate()}/${this.date.getMonth() + 1}/${this.date.getFullYear()}`);
-      let res = await fetch(".netlify/functions/getpage?url=" + url, { method: "post" });
+      let url = encodeURIComponent(
+        `student/Week.asp?term=4110&career=UGRD&dt=${this.date.getDate()}/${
+          this.date.getMonth() + 1
+        }/${this.date.getFullYear()}`
+      );
+      let res = await fetch(".netlify/functions/getpage?url=" + url, {
+        method: "post",
+      });
       if (res.status == 500) {
         this.errorText = await res.text();
         this.$refs.topProgress.fail();
@@ -43,18 +78,38 @@ export default {
       } else {
         this.$refs.topProgress.done();
 
-        let element = document.createElement('html');
-        res.text().then(body => this.timetable = body);
-        // res.text().then(body => element.innerHTML = body);
-        console.log(element);
-        // this.timetable = element.getElementsByTagName('table')[0];
+        let element = document.createElement("html");
+        res.text().then((body) => {
+          element.innerHTML = body;
+
+          let table = element.getElementsByTagName("table")[18];
+          console.log(table);
+          // this.timetable = table;
+
+          document.getElementById("badidea").innerHTML = table.outerHTML;
+          // this.timetable = element.getElementsByClassName('tbdr');
+
+          var classes = document.getElementsByClassName("altbdr");
+
+          for (var i = 0; i < classes.length; i++) {
+            let name = classes[i]
+              .getElementsByTagName("span")[0]
+              .innerText.split("\n")[1]; // class name (e.g. Music Technology Foundations)
+
+            let random_num = new seedrandom(name);
+            let hue = Math.floor(random_num.quick() * 360);
+            let colour = `hsl(${hue.toString()}, 100%, 80%)`;
+
+            classes[i].style.backgroundColor = colour;
+          }
+        });
       }
     },
   },
   data() {
     return {
       date: new Date(),
-      timetable: ""
+      timetable: "",
     };
   },
 };
