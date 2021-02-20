95103e6120d7b697b1d8bd33764719a4ed34ca2b
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2021-02-20 22:49:38 +1030

    More appropriate iconPaths

--- vue.config.js
+++ vue.config.js
@@ -6,54 +6,18 @@ module.exports = {
     });
   },
   pwa: {
-      short_name: "Timetable",
-      name: "Uni Timetable",
-      background_color: "#ffffff",
-      theme_color: "#6797ef",
-      display: "standalone", 
-      start_url: '/',
-      iconPaths: [
-          {
-              "src": "static/android-icon-36x36.png",
-              "sizes": "36x36",
-              "type": "image/png",
-              "density": "0.75"
-          },
-          {
-              "src": "static/android-icon-48x48.png",
-              "sizes": "48x48",
-              "type": "image/png",
-              "density": "1.0"
-          },
-          {
-              "src": "static/android-icon-72x72.png",
-              "sizes": "72x72",
-              "type": "image/png",
-              "density": "1.5"
-          },
-          {
-              "src": "static/android-icon-96x96.png",
-              "sizes": "96x96",
-              "type": "image/png",
-              "density": "2.0"
-          },
-          {
-              "src": "static/android-icon-144x144.png",
-              "sizes": "144x144",
-              "type": "image/png",
-              "density": "3.0"
-          },
-          {
-              "src": "static/android-icon-192x192.png",
-              "sizes": "192x192",
-              "type": "image/png",
-              "density": "4.0"
-          },
-          {
-              "src": "static/splash-screen-icon-512x512.png",
-              "sizes": "512x512",
-              "type": "image/png"
-          }
-      ]
+    short_name: "Timetable",
+    name: "Uni Timetable",
+    background_color: "#ffffff",
+    theme_color: "#6797ef",
+    display: "standalone",
+    start_url: '/',
+    iconPaths: {
+      favicon32: 'static/favicon-32x32.png',
+      favicon16: 'static/favicon-16x16.png',
+      appleTouchIcon: 'static/apple-icon-152x152.png',
+      maskIcon: 'static/splash-screen-icon-512x512.png',
+      msTileImage: 'static/ms-icon-144x144.png'
+    }
   }
 };
