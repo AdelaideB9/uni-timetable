7a6b2f90a61c89f6a47663ece40fceca45475e8d
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2021-02-20 22:10:18 +1030

    vue add pwaaaa

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Buefy from "buefy";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 Vue.use(Buefy, { defaultIconPack: "fas" });
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].title = "Uni Timetable";
-                return args;
-            })
-    }
-}
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "Uni Timetable";
+      return args;
+    });
+  }
+};
